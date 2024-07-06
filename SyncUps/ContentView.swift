3f3d05107017e7cffb2725a90f20380c6c86a3e6
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-07-06 22:12:09 +0900

    Initial Commit

---
+++ SyncUps/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SyncUps
+//
+//  Created by Yamamoto Kyo on 2024/07/06.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SyncUps/SyncUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsApp.swift
+//  SyncUps
+//
+//  Created by Yamamoto Kyo on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
